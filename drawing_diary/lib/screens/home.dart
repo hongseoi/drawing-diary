9d127f95ace14ee808e6bc4ceea4b5e1fe8d12f4
seoi hong <90823683+hongseoi@users.noreply.github.com>
2023-02-16 20:56:49 +0900

    fix files

---
+++ drawing_diary/lib/screens/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: 
+        Row(
+          children: [
+            Text('안녕')
+          ],
+        )
+    );
+  }
+}
